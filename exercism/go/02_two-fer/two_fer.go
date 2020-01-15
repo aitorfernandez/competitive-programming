696f5aace33ec0380fe4eab77a97e0ddacf5c41c
Aitor <helloimaitor@gmail.com>
2020-01-15 23:24:35 +0000

    a bit of DRY

--- exercism/go/02_two-fer/two_fer.go
+++ exercism/go/02_two-fer/two_fer.go
@@ -1,11 +1,15 @@
+// Package twofer create a sentence of the form "One for X, one for me.".
 package twofer
 
 import "fmt"
 
 // ShareWith returns a string with the message One for X, one for me.
 func ShareWith(name string) string {
+	var res string
 	if name == "" {
-		return fmt.Sprint("One for you, one for me.")
+		res = "you"
+	} else {
+		res = name
 	}
-	return fmt.Sprintf("One for %v, one for me.", name)
+	return fmt.Sprintf("One for %s, one for me.", res)
 }
