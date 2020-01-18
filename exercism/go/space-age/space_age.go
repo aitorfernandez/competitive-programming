966aef9c89d49ed02dcedbd337388be7e0b3cc49
Aitor <helloimaitor@gmail.com>
2020-01-18 10:33:20 +0000

    remove function

--- exercism/go/space-age/space_age.go
+++ exercism/go/space-age/space_age.go
@@ -5,31 +5,21 @@ type Planet string
 
 const earthSeconds = 365.25 * 24 * 60 * 60
 
-// Age calculate how old someone would be in other planet
-func Age(s float64, p Planet) float64 {
-	o := orbitalPeriod(p)
-	return s / (o * earthSeconds)
+var planets = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
-func orbitalPeriod(p Planet) float64 {
-	switch p {
-	case "Earth":
-		return 1
-	case "Mercury":
-		return 0.2408467
-	case "Venus":
-		return 0.61519726
-	case "Mars":
-		return 1.8808158
-	case "Jupiter":
-		return 11.862615
-	case "Saturn":
-		return 29.447498
-	case "Uranus":
-		return 84.016846
-	case "Neptune":
-		return 164.79132
-	default:
-		return 0
+// Age calculate how old someone would be in other planet
+func Age(s float64, p Planet) float64 {
+	if val, ok := planets[p]; ok {
+		return s / (val * earthSeconds)
 	}
+	return 0
 }
