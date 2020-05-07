28a0c194be3449f6a7c71b23cc898d1e540451e3
Aitor <helloimaitor@gmail.com>
2020-05-07 17:02:09 +0100

    add peak-valley approach exercise

---
+++ time-to-buy-and-sell/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+var testData = []struct {
+	input  []int
+	output int
+}{
+	{
+		[]int{7, 1, 5, 3, 6, 4},
+		7,
+	},
+	{
+		[]int{1, 2, 3, 4, 5},
+		4,
+	},
+	{
+		[]int{7, 6, 4, 3, 1},
+		0,
+	},
+}
+
+func TestMaxProfict(t *testing.T) {
+	for _, test := range testData {
+		got := maxProfit(test.input)
+		if got != test.output {
+			t.Fatalf("got %v, want %v for input %v", got, test.output, test.input)
+		}
+	}
+}
