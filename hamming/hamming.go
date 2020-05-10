08e8720e5398241b0261c2cd3e10f205187b38be
Aitor <helloimaitor@gmail.com>
2020-05-10 15:19:08 +0100

    remove empty check

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -11,15 +11,11 @@ func Distance(a, b string) (int, error) {
 	ar, br := []rune(a), []rune(b)
 
 	if len(ar) != len(br) {
-		return 0, fmt.Errorf("sequences not equal %v, %v", len(a), len(b))
-	}
-
-	if a == "" && b == "" {
-		return 0, nil
+		return 0, fmt.Errorf("sequences not equal %v, %v", a, b)
 	}
 
 	var counter int
-	for i := 0; i < len(ar); i++ {
+	for i := range ar {
 		if ar[i] != br[i] {
 			counter++
 		}
