015fcbea4d1ae5dbf6f3ca955d1da14e20ac3bcc
Aitor <helloimaitor@gmail.com>
2020-08-03 13:59:25 +0100

    update exercise to use switch

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -31,16 +31,6 @@ import (
 	"strings"
 )
 
-var letters = map[int][]string{
-	1:  []string{"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"},
-	2:  []string{"d", "g"},
-	3:  []string{"b", "c", "m", "p"},
-	4:  []string{"f", "h", "v", "w", "y"},
-	5:  []string{"k"},
-	8:  []string{"j", "x"},
-	10: []string{"q", "z"},
-}
-
 // Score returns the Scrabble score.
 func Score(str string) int {
 	var score int
@@ -49,24 +39,24 @@ func Score(str string) int {
 		return score
 	}
 
-	str = strings.ToLower(str)
-
 	for _, s := range str {
-		for k, v := range letters {
-			if in(v, string(s)) {
-				score += k
-			}
+		switch strings.ToLower(string(s)) {
+		case "a", "e", "i", "o", "u", "l", "n", "r", "s", "t":
+			score += 1
+		case "d", "g":
+			score += 2
+		case "b", "c", "m", "p":
+			score += 3
+		case "f", "h", "v", "w", "y":
+			score += 4
+		case "k":
+			score += 5
+		case "j", "x":
+			score += 8
+		case "q", "z":
+			score += 10
 		}
 	}
 
 	return score
-}
-
-func in(ss []string, v string) bool {
-	for _, s := range ss {
-		if s == v {
-			return true
-		}
-	}
-	return false
 }
