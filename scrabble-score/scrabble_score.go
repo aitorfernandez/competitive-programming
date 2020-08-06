78f98e500e1797e108bf24c67522b043a258bfb1
Aitor <helloimaitor@gmail.com>
2020-08-06 10:59:55 +0100

    udpate quotes for single rune

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -34,20 +34,20 @@ func Score(str string) int {
 	var score int
 
 	for _, s := range str {
-		switch string(unicode.ToLower(s)) {
-		case "a", "e", "i", "o", "u", "l", "n", "r", "s", "t":
+		switch unicode.ToLower(s) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
 			score += 1
-		case "d", "g":
+		case 'd', 'g':
 			score += 2
-		case "b", "c", "m", "p":
+		case 'b', 'c', 'm', 'p':
 			score += 3
-		case "f", "h", "v", "w", "y":
+		case 'f', 'h', 'v', 'w', 'y':
 			score += 4
-		case "k":
+		case 'k':
 			score += 5
-		case "j", "x":
+		case 'j', 'x':
 			score += 8
-		case "q", "z":
+		case 'q', 'z':
 			score += 10
 		}
 	}
