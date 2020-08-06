eeab8d661dfb8aeaba3c506c57e09dcda5255e9e
Aitor <helloimaitor@gmail.com>
2020-08-06 20:09:03 +0100

    update if chain with a switch

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -24,31 +24,28 @@ const (
 // KindFromSides determine type of triangle.
 func KindFromSides(a, b, c float64) Kind {
 	// isValid check for a shape to be a triangle.
-	isValid := func(v float64) bool {
-		if math.IsNaN(v) || math.IsInf(v, 0) || v <= 0 {
-			return false
-		}
-		return true
-	}
+	// isValid := func(v float64) bool {
+	// 	if math.IsNaN(v) || math.IsInf(v, 0) || v <= 0 {
+	// 		return false
+	// 	}
+	// 	return true
+	// }
 
-	ss := []float64{a, b, c}
-	for _, s := range ss {
-		if !isValid(s) {
-			return NaT
-		}
-	}
+	// ss := []float64{a, b, c}
+	// for _, s := range ss {
+	// 	if !isValid(s) {
+	// 		return NaT
+	// 	}
+	// }
 
-	if a+b < c || b+c < a || a+c < b {
+	switch {
+	case a*b*c <= 0, a+b < c, b+c < a, a+c < b, math.IsInf(a+b+c, 1), math.IsNaN(a + b + c):
 		return NaT
-	}
-
-	if a == b && b == c {
+	case a == b && b == c:
 		return Equ
-	}
-
-	if a == b || b == c || c == a {
+	case a == b, b == c, c == a:
 		return Iso
+	default:
+		return Sca
 	}
-
-	return Sca
 }
