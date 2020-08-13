152ce5a1762db46a2740be94315aeaff9b6eb1aa
Aitor <helloimaitor@gmail.com>
2020-08-13 20:24:13 +0100

    improve permutation exercise

--- permutation/permutation.go
+++ permutation/permutation.go
@@ -3,31 +3,49 @@ Package permutation decide is one string is permutation of the other.
 */
 package permutation
 
-import (
-	"sort"
-	"strings"
-)
-
 // Permutation check if two strings are permutation.
 func Permutation(a, b string) bool {
 	if len(a) != len(b) {
 		return false
 	}
 
-	aa := splitAndSort(a)
-	bb := splitAndSort(b)
+	m := make(map[rune]int)
+	for _, r := range a {
+		m[r]++
+	}
 
-	for i := range aa {
-		if aa[i] != bb[i] {
+	for _, r := range b {
+		if _, ok := m[r]; !ok {
+			return false
+		}
+		m[r]--
+		if m[r] < 0 {
 			return false
 		}
 	}
+
 	return true
 }
 
-func splitAndSort(s string) []string {
-	arr := strings.Split(s, "")
-	sort.Strings(arr)
+// func Permutation(a, b string) bool {
+// 	if len(a) != len(b) {
+// 		return false
+// 	}
 
-	return arr
-}
+// 	aa := splitAndSort(a)
+// 	bb := splitAndSort(b)
+
+// 	for i := range aa {
+// 		if aa[i] != bb[i] {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// func splitAndSort(s string) []string {
+// 	arr := strings.Split(s, "")
+// 	sort.Strings(arr)
+
+// 	return arr
+// }
