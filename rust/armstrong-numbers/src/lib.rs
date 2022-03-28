6f3358324eeed63cd9492fa618b72ebcd4123eca
Aitor Fernandez <helloimaitor@gmail.com>
2022-03-28 17:17:34 +0100

    reset rust track

--- rust/armstrong-numbers/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    let num_str = num.to_string();
-    num_str
-        .chars()
-        .filter_map(|c| c.to_digit(10))
-        .map(|d| d.pow(num_str.len() as u32))
-        .sum::<u32>()
-        == num
-}
--- rust/gigasecond/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use time::{Duration, PrimitiveDateTime as DateTime};
-
-pub fn after(start: DateTime) -> DateTime {
-    let x: i64 = 10;
-    start + (Duration::seconds(x.pow(9)))
-}
