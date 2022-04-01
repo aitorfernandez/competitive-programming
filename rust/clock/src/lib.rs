717ec3a1fcb867d6688eec2f816fe1b2ea470104
Aitor Fernandez <helloimaitor@gmail.com>
2022-04-01 18:04:53 +0100

    prepare struct

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.hours, self.minutes)
+    }
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        unimplemented!(
+            "Construct a new Clock from {} hours and {} minutes",
+            hours,
+            minutes
+        );
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    }
+}
