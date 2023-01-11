e7f9809692bca94ec6d11f79dfcbe9fd656741ba
aitorfernandez <helloimaitor@gmail.com>
2023-01-11 13:59:19 +0000

    beginning again

--- rust/armstrong-numbers/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    let num_str: String = num.to_string();
-    num_str
-        .chars()
-        .filter_map(|c| c.to_digit(10))
-        .map(|n| n.pow(num_str.len() as u32))
-        .sum::<u32>()
-        == num
-}
