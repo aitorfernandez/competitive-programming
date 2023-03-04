9b151ae2878ebe063cbed6ab404633f1e658bc98
aitorfernandez <helloimaitor@gmail.com>
2023-03-04 16:54:20 +0000

    push and pop

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -4,19 +4,22 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     for c in string.chars() {
         match c {
             '[' | '{' | '(' => stack.push(c),
-            ']' | '}' | ')' => {
-                if let Some(last) = stack.pop() {
-                    if (c == ']' && last != '[')
-                        || (c == '}' && last != '{')
-                        || (c == ')' && last != '(')
-                    {
-                        return false;
-                    }
-                } else {
+            ']' => {
+                if stack.pop() != Some('[') {
                     return false;
                 }
             }
-            _ => continue,
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            _ => (),
         }
     }
 
