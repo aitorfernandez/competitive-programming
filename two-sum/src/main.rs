254b9bf1a8577c564180bd6ec05069a3597b9965
aitorfernandez <helloimaitor@gmail.com>
2023-03-21 22:20:45 +0000

    HashMap, insert and get with match

--- two-sum/src/main.rs
+++ two-sum/src/main.rs
@@ -1,43 +1,22 @@
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     use std::collections::HashMap;
-    let mut hm: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
+
+    let mut hm: HashMap<i32, i32> = HashMap::new();
 
     for (i, n) in nums.iter().enumerate() {
         match hm.get(&(target - n)) {
             Some(v) => return vec![*v, i as i32],
-            None => hm.insert(*n, i as i32),
-        };
+            None => {
+                hm.insert(*n, i as i32);
+            }
+        }
     }
 
     vec![]
 }
 
 fn main() {
-    println!("two-sum",);
-}
-
-#[test]
-fn two_sum_test() {
-    struct TestData {
-        nums: Vec<i32>,
-        target: i32,
-        res: Vec<i32>,
-    }
-
-    for t in vec![
-        TestData {
-            nums: vec![2, 7, 11, 15],
-            target: 9,
-            res: vec![0, 1],
-        },
-        TestData {
-            nums: vec![5, 6, 3, 9],
-            target: 8,
-            res: vec![0, 2],
-        },
-    ] {
-        let res = two_sum(t.nums, t.target);
-        assert_eq!(res.len(), 2);
-        assert_eq!(res, t.res);
-    }
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
 }
