dd4b4174a8c0b154598bea7b294746ec454f4865
aitorfernandez <foraitorfernandez+github@gmail.com>
2024-02-04 14:32:10 +0000

    rethinking

--- palindrome/src/main.rs
+++
@@ -1,8 +0,0 @@
-pub fn is_palindrome(x: i32) -> bool {
-    x.to_string().chars().rev().eq(x.to_string().chars())
-}
-
-fn main() {
-    assert!(is_palindrome(121));
-    assert!(!is_palindrome(10));
-}
