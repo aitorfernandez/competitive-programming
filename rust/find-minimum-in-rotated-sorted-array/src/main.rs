51450432e12b6724938389f5f918df267e01b69f
aitorfernandez <foraitorfernandez+github@gmail.com>
2024-09-04 19:13:00 +0100

    update binary search conditions on find minimum in sorted array

--- rust/find-minimum-in-rotated-sorted-array/src/main.rs
+++ rust/find-minimum-in-rotated-sorted-array/src/main.rs
@@ -8,29 +8,34 @@ fn find_min(nums: Vec<i32>) -> i32 {
     while r > l {
         let m = l + (r - l) / 2;
 
-        let left = nums[l];
-        let mid = nums[m];
-        let right = nums[r];
-
-        if left <= mid && mid <= right {
-            return left;
-        } else if left >= mid && mid >= right {
-            return right;
-        } else if left > mid || mid < right {
+        if nums[m] < nums[r] {
             r = m;
         } else {
-            l = m;
+            l = m + 1;
         }
+
+        // let left = nums[l];
+        // let mid = nums[m];
+        // let right = nums[r];
+
+        // if left <= mid && mid <= right {
+        //     return left;
+        // } else if left >= mid && mid >= right {
+        //     return right;
+        // } else if left > mid || mid < right {
+        //     r = m;
+        // } else {
+        //     l = m;
+        // }
     }
 
-    -1
+    nums[r]
 }
 
 fn main() {
     assert_eq!(find_min(vec![5, 1, 2, 3, 4]), 1);
-
-    // assert_eq!(find_min(vec![3, 4, 5, 1, 2]), 1);
-    // assert_eq!(find_min(vec![4, 5, 6, 7, 0, 1, 2]), 0);
-    // assert_eq!(find_min(vec![11, 13, 15, 17]), 11);
-    // assert_eq!(find_min(vec![1]), 1);
+    assert_eq!(find_min(vec![3, 4, 5, 1, 2]), 1);
+    assert_eq!(find_min(vec![4, 5, 6, 7, 0, 1, 2]), 0);
+    assert_eq!(find_min(vec![11, 13, 15, 17]), 11);
+    assert_eq!(find_min(vec![1]), 1);
 }
