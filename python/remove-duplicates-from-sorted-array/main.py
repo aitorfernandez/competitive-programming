82d188477295853bbea8ea6bcffaf56cc800abe0
aitorfernandez <foraitorfernandez+github@gmail.com>
2024-09-08 16:43:36 +0100

    naming update

--- python/remove-duplicates-from-sorted-array/main.py
+++ python/remove-duplicates-from-sorted-array/main.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def removeDuplicates(nums: List[int]) -> int:
+def remove_duplicate(nums: List[int]) -> int:
     l = 1
 
     for r in range(1, len(nums)):
@@ -13,7 +13,7 @@ def removeDuplicates(nums: List[int]) -> int:
 
 
 if __name__ == "__main__":
-    assert (res := removeDuplicates([1, 1, 2])) == 2, f"Expected 2, got {res}"
+    assert (res := remove_duplicate([1, 1, 2])) == 2, f"Expected 2, got {res}"
     assert (
-        res := removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+        res := remove_duplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
     ) == 5, f"Expected 5, got {res}"
