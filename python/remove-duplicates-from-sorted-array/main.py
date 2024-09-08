07b8d0c230b9f31b384707dba70cebbcdf4919ed
aitorfernandez <foraitorfernandez+github@gmail.com>
2024-09-08 16:36:25 +0100

    two pointers for remove duplicates

---
+++ python/remove-duplicates-from-sorted-array/main.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def removeDuplicates(nums: List[int]) -> int:
+    l = 1
+
+    for r in range(1, len(nums)):
+        if nums[r] != nums[r - 1]:
+            nums[l] = nums[r]
+            l += 1
+
+    return l
+
+
+if __name__ == "__main__":
+    assert (res := removeDuplicates([1, 1, 2])) == 2, f"Expected 2, got {res}"
+    assert (
+        res := removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+    ) == 5, f"Expected 5, got {res}"
