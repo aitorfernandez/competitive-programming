6c013352e1fe4fc81660d9d6bcb7217160b30db0
aitorfernandez <foraitorfernandez+github@gmail.com>
2024-11-01 11:19:07 +0000

    review dp for climbing stairs

--- leetcode/climbing-stairs/main.py
+++ leetcode/climbing-stairs/main.py
@@ -1,18 +1,27 @@
 def climbStairs(n: int) -> int:
-    memo = {}
+    dp = [0, 1]
 
-    def dfs(i):
-        if i == n:
-            return 1
-        if i > n:
-            return 0
-        if i in memo:
-            return memo[i]
+    for _ in range(n - 1):
+        temp = dp[0] + dp[1]
+        dp[0] = dp[1]
+        dp[1] = temp
 
-        memo[i] = dfs(i + 1) + dfs(i + 2)
-        return memo[i]
+    return dp[0] + dp[1]
 
-    return dfs(0)
+    # memo = {}
+
+    # def dfs(i):
+    #     if i == n:
+    #         return 1
+    #     if i > n:
+    #         return 0
+    #     if i in memo:
+    #         return memo[i]
+
+    #     memo[i] = dfs(i + 1) + dfs(i + 2)
+    #     return memo[i]
+
+    # return dfs(0)
 
 
 if __name__ == "__main__":
@@ -24,3 +33,5 @@ if __name__ == "__main__":
     # 1. 1 step + 1 step + 1 step
     # 2. 1 step + 2 steps
     # 3. 2 steps + 1 step
+
+    assert climbStairs(38) == 63245986
